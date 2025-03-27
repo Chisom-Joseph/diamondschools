e7ef1aacccb6c8a3d7a7cf11b89e4ce1f3e6781a
Chisom Joseph Njoku <njokuchisomjoseph@gmail.com>
2025-03-27 22:06:51 +0100

    Feat: Add aspirantExaminationDate field to ExamSettings model and update getExaminationDate utility to retrieve formatted examination date

--- utils/getExaminationDate.js
+++ utils/getExaminationDate.js
@@ -1,10 +1,22 @@
 const logger = require("../utils/logger");
+const { ExamSettings } = require("../models");
+const moment = require("moment");
 
 module.exports = async () => {
   try {
-    return "Saturday 2nd December 2024";
+    const examSettings = await ExamSettings.findOne({});
+    const { aspirantExaminationDate } = examSettings;
+
+    if (!aspirantExaminationDate) throw new Error("No examination date found.");
+
+    const formattedDate = moment(aspirantExaminationDate).format(
+      "dddd Do MMMM YYYY"
+    );
+
+    return formattedDate;
   } catch (error) {
     logger.error(`ERROR GETTING EXAMINATION DATE`);
     logger.error(error.stack);
+    return null;
   }
 };
